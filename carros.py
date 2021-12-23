b9b8a1bddf782ed57250ef4bb7a314085b3862ed
gabiqueiroga <gabriela.queiroga@aluno.ufca.edu.br>
2021-12-22 23:30:49 -0300

    Melhoria na organizacao dos carros

--- carros.py
+++ carros.py
@@ -1,50 +1,48 @@
 import pygame as pg
 
-# imagens de carros no metodo pg.image.load("carroNovo.png")
-
 class Carro(pg.sprite.Sprite):
     def __init__(self, quantidade, nivel):
         super().__init__()
-        # normal
+
         if nivel == 1:
             if quantidade == 1:
-                self.x = 250
+                self.x = 290
                 self.image = pg.image.load("carroNovo.png")
-                self.velo = -4
+                self.velo = -1
 
             elif quantidade == 2:
-                self.x = 350
+                self.x = 375
                 self.image = pg.image.load("carroNovo.png")
-                self.velo = 5
+                self.velo = -2
             elif quantidade == 3:
-                self.x = 450
+                self.x = 575
                 self.image = pg.image.load("carroNovo.png")
-                self.velo = 3
+                self.velo = -3
             else:
-                self.x = 580
+                self.x = 660
                 self.image = pg.image.load("carroNovo.png")
                 self.velo = -2
         else:
             if quantidade == 1:
-                self.x = 250
+                self.x = 290
+                self.image = pg.image.load("carroNovo.png")
+                self.velo = -2
+
+            elif quantidade == 2:
+                self.x = 370
+                self.image = pg.image.load("carroNovo.png")
+                self.velo = -3
+            elif quantidade == 3:
+                self.x = 575
+                self.image = pg.image.load("carroNovo.png")
+                self.velo = -5
+            else:
+                self.x = 660
                 self.image = pg.image.load("carroNovo.png")
                 self.velo = -3
 
-            elif quantidade == 2:
-                self.x = 350
-                self.image = pg.image.load("carroNovo.png")
-                self.velo = 3
-            elif quantidade == 3:
-                self.x = 450
-                self.image = pg.image.load("carroNovo.png")
-                self.velo = 6
-            else:
-                self.x = 580
-                self.image = pg.image.load("carroNovo.png")
-                self.velo = -5
-    # facil
         self.y = 650/2
-        self.width = 100
+        self.width = 140
         self.height = 150
         self.image = pg.transform.scale(self.image, (self.width, self.height))
         self.mask = pg.mask.from_surface(self.image)
