0e447b35271d4d38cc2ca1f251897f5c3f633b11
AdrianaSil <96041964+AdrianaSil@users.noreply.github.com>
2022-01-05 21:59:43 -0300

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,33 @@
 from utils import *
 
+def test_botoes():
+    mx, my = pg.mouse.get_pos()  
+    click = False
+
+    iniciar = pg.Rect(300, 150, 240, 55)
+    ajuda = pg.Rect(300, 250, 240, 55)
+    sair = pg.Rect(300, 350, 240, 55)
+    retornarmenu = pg.Rect(300, 500, 240, 55)
+
+    for event in pg.event.get():
+        if event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                click = True
+
+    if iniciar.collidepoint((mx, my)):
+        if click:
+            assert True
+    if ajuda.collidepoint((mx, my)):
+        if click:
+            assert True
+    if sair.collidepoint((mx, my)):
+        if click:
+            assert True
+    if retornarmenu.collidepoint((mx, my)):
+        if click:
+            assert True
+
+
 def test_teclaSecreta():
     teclaSecreta()
     assert galinha.x == LARGURA + 200
